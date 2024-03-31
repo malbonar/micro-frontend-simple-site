ee969e07fb54a9d572c5cb2e83d4f16755f26e7e
Malcolm Bonar <malbonar@gmail.com>
2024-04-01 12:57:37 +1300

    Change to trigger github action

--- container/src/components/MarketingAppComponent.js
+++ container/src/components/MarketingAppComponent.js
@@ -3,6 +3,7 @@ import { mount } from 'marketing/Marketing';
 import PageMount from './shared/PageMount';
 
 export const MarketingAppComponent = () => {
+    const bob = 'bob';
     return <PageMount mount={mount} />
 }
 
