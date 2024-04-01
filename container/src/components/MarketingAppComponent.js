c28ac2f1c137352c5be57703088b5bce387201eb
Malcolm Bonar <malbonar@gmail.com>
2024-04-01 18:43:51 +1300

    Fixed AWS key name in container github action file

--- container/src/components/MarketingAppComponent.js
+++ container/src/components/MarketingAppComponent.js
@@ -3,7 +3,6 @@ import { mount } from 'marketing/Marketing';
 import PageMount from './shared/PageMount';
 
 export const MarketingAppComponent = () => {
-    const bob = 'bob';
     return <PageMount mount={mount} />
 }
 
