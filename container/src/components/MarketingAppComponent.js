582d3fb9bcf06098f32066362686a22fa9a74bf7
Malcolm Bonar <malbonar@gmail.com>
2024-04-01 23:07:03 +1300

    Commit to trigger a github action build after making a change to the github action file

--- container/src/components/MarketingAppComponent.js
+++ container/src/components/MarketingAppComponent.js
@@ -3,7 +3,7 @@ import { mount } from 'marketing/Marketing';
 import PageMount from './shared/PageMount';
 
 export const MarketingAppComponent = () => {
-    const bump = 'commit to trigger a build...';
+    const bump = 'commit to trigger a build..';
     return <PageMount mount={mount} />
 }
 
