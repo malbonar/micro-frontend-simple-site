c1f784812b53fd4d9388f5c3e2956e4a65818d22
Malcolm Bonar <malbonar@gmail.com>
2024-04-01 18:52:37 +1300

    Commit to trigger a container build

--- container/src/components/MarketingAppComponent.js
+++ container/src/components/MarketingAppComponent.js
@@ -3,6 +3,7 @@ import { mount } from 'marketing/Marketing';
 import PageMount from './shared/PageMount';
 
 export const MarketingAppComponent = () => {
+    const bump = 'commit to trigger a build...';
     return <PageMount mount={mount} />
 }
 
