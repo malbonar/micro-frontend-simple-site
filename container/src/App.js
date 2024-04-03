00bf76a42d3ec371e69c59fe8c769fc93def010d
Malcolm Bonar <malbonar@gmail.com>
2024-04-03 18:40:38 +1300

    Added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { MarketingAppComponent } from './components/MarketingAppComponent';
+import Header from './components/Header';
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Header />
             <Switch>
                 <Route exact path="/" component={MarketingAppComponent} />
             </Switch>
