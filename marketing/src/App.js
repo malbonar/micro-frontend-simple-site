2326783bd1157c1c23ed3bbfd57542b601855102
Malcolm Bonar <malbonar@gmail.com>
2024-04-03 23:21:12 +1300

    Fixed CSS class name clash in production

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
+// prevent this module and another e.g. container having the same shortened class names in prod
+const generatedClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+});
 
 export default () => {
     return (
-        <StylesProvider>
+        <StylesProvider generateClassName={generatedClassName}>
             <BrowserRouter>
                 <Switch> 
                     <Route path="/pricing" component={Pricing} />
